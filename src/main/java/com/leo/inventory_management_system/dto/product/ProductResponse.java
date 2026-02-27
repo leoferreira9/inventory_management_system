a08f13c87c95f51d697a91166c285d2a4504f540
Leonardo <leobarros_@outlook.com>
2026-02-26 21:36:20 -0300

    feat: add setters to ProductResponse DTO

--- src/main/java/com/leo/inventory_management_system/dto/product/ProductResponse.java
+++ src/main/java/com/leo/inventory_management_system/dto/product/ProductResponse.java
@@ -10,7 +10,7 @@ public class ProductResponse {
     private String description;
     private BigDecimal price;
     private String sku;
-    private boolean active;
+    private Boolean active;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
@@ -20,31 +20,63 @@ public class ProductResponse {
         return id;
     }
 
+    public void setId(Long id){
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description){
+        this.description = description;
+    }
+
     public BigDecimal getPrice() {
         return price;
+    }
+
+    public void setPrice(BigDecimal price){
+        this.price = price;
     }
 
     public String getSku() {
         return sku;
     }
 
-    public boolean isActive() {
+    public void setSku(String sku){
+        this.sku = sku;
+    }
+
+    public Boolean isActive() {
         return active;
+    }
+
+    public void setActive(Boolean active){
+        this.active = active;
     }
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
+    }
+
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt){
+        this.updatedAt = updatedAt;
     }
 }
