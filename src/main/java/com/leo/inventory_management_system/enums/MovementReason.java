7ac69ecaa54b7a75af83897b94348d838ae8261e
Leonardo <leobarros_@outlook.com>
2026-03-03 00:13:20 -0300

    feat: add MovementReason enum

---
+++ src/main/java/com/leo/inventory_management_system/enums/MovementReason.java
@@ -0,0 +1,12 @@
+package com.leo.inventory_management_system.enums;
+
+public enum MovementReason {
+    PURCHASE,
+    INITIAL_STOCK,
+    RETURN_FROM_CLIENT,
+    SALE,
+    EXPIRED,
+    DAMAGE,
+    ADJUSTMENT_IN,
+    ADJUSTMENT_OUT
+}
